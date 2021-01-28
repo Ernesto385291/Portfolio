d8e9690fb484e8d0e0191a02beed190dadd29e4c
Ernesto Vizcaíno <nene09991@gmail.com>
2021-01-28 16:01:38 -0600

    Added analitycs

--- pages/_document.js
+++ pages/_document.js
@@ -1,19 +1,40 @@
-import Document from "next/document"
-import { ServerStyleSheet } from "styled-components"
+import Document, { Head } from "next/document";
+import { Fragment } from "react";
+import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const sheet = new ServerStyleSheet()
-    const originalRenderPage = ctx.renderPage
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
     try {
       ctx.renderPage = () =>
         originalRenderPage({
           enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />),
-        })
+            sheet.collectStyles(
+              <Fragment>
+                <Head>
+                  <script
+                    async
+                    src="https://www.googletagmanager.com/gtag/js?id=G-K6LDVC7P76"
+                  ></script>
+                  <script>
+                    dangerouslySetInnerHTML=
+                    {{
+                      __html: `window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+            
+              gtag('config', G-K6LDVC7P76);`,
+                    }}
+                  </script>
+                </Head>
+                <App {...props} />
+              </Fragment>
+            ),
+        });
 
-      const initialProps = await Document.getInitialProps(ctx)
+      const initialProps = await Document.getInitialProps(ctx);
       return {
         ...initialProps,
         styles: (
@@ -22,9 +43,9 @@ export default class MyDocument extends Document {
             {sheet.getStyleElement()}
           </>
         ),
-      }
+      };
     } finally {
-      sheet.seal()
+      sheet.seal();
     }
   }
 }
